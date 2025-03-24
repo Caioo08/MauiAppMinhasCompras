cf7804164fd481c2a898fd2500790e8565db4171
231098 <231098@CPD03-M16>
2025-03-24 10:30:39 -0300

    .

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MauiAppMinhasCompras.Helpers;
+﻿using System.Globalization;
+using MauiAppMinhasCompras.Helpers;
 
 namespace MauiAppMinhasCompras
 {
@@ -26,8 +27,10 @@ namespace MauiAppMinhasCompras
         public App()
         {
             InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
 }
